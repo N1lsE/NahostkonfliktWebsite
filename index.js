4c8e712e7a33fd89e2ea32577c7d80b1bbbb2327
Nils <nilseckerle2001@gmail.com>
2023-11-02 10:05:50 +0100

    erase files to start over
    erase files to start over by following a YT tutorial: https://www.youtube.com/watch?v=t5AE66WgQD0

--- index.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById('zeitleiste').innerHTML = '<ol><li>html data</li></ol>';
-
-var fr = new FileReader()
-var c = fr.readAsText("events/2000.10.24.txt", "UTF-8")
-console.log(c)
